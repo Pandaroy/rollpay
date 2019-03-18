9bab7475d89ec2cb5b9ccc60eee9c43c3b1a2c9a
pandaroy <royette.vertudazo@gmail.com>
2019-03-18 14:56:13 +0800

    Added main class

---
+++ src/app-web/rollpaymain/src/main/java/com/rollpay/rollpay.java
@@ -0,0 +1,13 @@
+package com.rollpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class rollpay {
+
+	public static void main(String[] args) {
+		SpringApplication.run(rollpay.class);
+	}
+
+}
